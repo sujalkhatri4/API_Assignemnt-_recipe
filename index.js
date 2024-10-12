0fc5274892130394cfdf718abbc1b0791a93f6f1
Sujal <sujalkhatri411@gmail.com>
2024-10-12 13:42:49 -0400

    imported the express and initizlie the port

---
+++ index.js
@@ -0,0 +1,10 @@
+//import express
+const express = require('express');
+
+//Initiazlie the port
+const PORT = 2004;
+
+// Initiazlize the sserver
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
