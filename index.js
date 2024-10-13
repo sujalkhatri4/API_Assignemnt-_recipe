abc3a6f882ada52538d1ae184a67b5608d49ebcf
Sujal <sujalkhatri411@gmail.com>
2024-10-13 01:06:34 -0400

    package installed mongoose

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 //import express
 const express = require('express');
-
+const bodyParser = require('body-parser');
 
 //init the express app
 const app = express();
+
+// Middleware
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended:true})); 
+
+//Define  a route 
+app.get ('/',(req,res)=>{
+    res.send('welcome to  my recipe assignemnt ')
+})
+
 
 //Initiazlie the port
 const PORT = 2004;
 
 // Initiazlize the sserver
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
