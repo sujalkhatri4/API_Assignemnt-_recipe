6550e266c644009f6c28df9dcf0aa7c9edb8e45d
Sujal <sujalkhatri411@gmail.com>
2024-10-13 01:20:37 -0400

    some changes

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 //import express
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 //init the express app
 const app = express();
+
+//Mongo atlas connection string
+const mongoURI = 'mongodb+srv://veroproduction4:sujalapi@cluster4.rmdge.mongodb.net/';
+
+//connect to mongodb Atlas 
+mongoose.connect(mongoURI)
+.then(()=>{
+    console.log('connected to  mongodb')
+})
+.catch((error)=>{
+    console.error('error connecting to mongoDb',error);
+});
 
 // Middleware
 app.use(bodyParser.json()); 
