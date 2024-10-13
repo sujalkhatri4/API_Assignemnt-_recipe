47c06e3fbed7a4c6cadca3119e21cfecda941704
Sujal <sujalkhatri411@gmail.com>
2024-10-13 02:25:17 -0400

    created db.js

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const fs = require('fs');
 dotenv.config({path: './config.env'});
+const InitiateMongoServer = require('./db');
+
+//Read data from Recipes.json
+const data = JSON.parse(fs.readFileSync('./Recipes.json','utf-8'));
+
+//Initialize Mongo Server
+InitiateMongoServer();
 
 //init the express app
 const app = express();
 
+
 //Mongo atlas connection string
 //const mongoURI = 'mongodb+srv://veroproduction4:sujalapi@cluster4.rmdge.mongodb.net/';
-
-//connect to mongodb Atlas 
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>{
-    console.log('connected to  mongodb')
-})
-.catch((error)=>{
-    console.error('error connecting to mongoDb',error);
-});
 
 // Middleware
 app.use(bodyParser.json()); 
