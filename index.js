063d213d6c712d953e84896309d62f97b2718b34
Sujal <sujalkhatri411@gmail.com>
2024-10-13 14:13:43 -0400

    error

--- index.js
+++ index.js
@@ -3,18 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const fs = require('fs');
 dotenv.config({path: './config.env'});
-const InitiateMongoServer = require('./db');
-const { route } = require('./src/routes/routes');
-const recipeRoutes = require('./src/routes/routes');
 
-//Read data from Recipes.json
-const data = JSON.parse(fs.readFileSync('./recipes.json','utf-8'));
-//console.log(data);
-
-//Initialize Mongo Server
-InitiateMongoServer();
 
 //init the express app
 const app = express();
@@ -28,16 +18,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true})); 
 
 
-
 //Define  a route 
 app.get ('/',(req,res)=>{
     res.send('welcome to  my recipe assignemnt ')
-
 })
-
-// Link the Routes
-app.use('/import/recipe',recipeRoutes);
-console.log("h1")
 
 
 //Initiazlie the port
