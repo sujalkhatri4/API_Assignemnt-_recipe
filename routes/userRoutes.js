c4a303011a6b7f4909d92f9b6fb36162b8e1d5ad
Sujal <sujalkhatri411@gmail.com>
2024-12-07 23:09:55 -0500

    created userRoutes.js

---
+++ routes/userRoutes.js
@@ -0,0 +1,16 @@
+
+
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+
+router.post('/register', userController.registerUser);
+
+
+router.post('/login', userController.loginUser);
+
+
+router.post('/logout', userController.logoutUser);
+
+module.exports = router;
